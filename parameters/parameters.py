40d1343370d6e1380b0b4bb6e8760f0fa2ba182d
Daniel Pierce <danp101@comcast.net>
2026-08-10 16:53:13 -0400

    Some minor reformatting

--- parameters/parameters.py
+++ parameters/parameters.py
@@ -4,10 +4,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Define Constants
+# Define Constants
 angles = np.deg2rad(np.array([15, 30, 45, 60, 75]))
 v_0 = 15
 g = 9.81
+
 
 def main():
     plot_trajectory(angles)
@@ -18,16 +19,19 @@ def plot_trajectory(angles):
     Takes as input a NumPy array of angles and creates a plot comparing the trajectories for all the angles provided as input.
     """
     fig, ax = plt.subplots()
+    ax.set_xlabel("Position (horizontal component)")
+    ax.set_ylabel("Position (vertical component)")
+    ax.set_title("Horizontal position vs. Vertical position")
+
     for angle in angles:
         final_time = flight_time(angle)
         t = np.linspace(0, final_time, 100)
-        x,y = trajectory(t, angle)
-        ax.set_xlabel("Position (horizontal component)")
-        ax.set_ylabel("Position (vertical component)")
-        ax.set_title("Horizontal position vs. Vertical position")
+        x, y = trajectory(t, angle)
         ax.plot(x, y, label=f"{round(np.rad2deg(angle))}\N{DEGREE SIGN}")
-        ax.legend()
+
+    ax.legend()
     plt.show()
+
 
 def flight_time(angle):
     """
@@ -37,6 +41,7 @@ def flight_time(angle):
 
     return t
 
+
 def trajectory(time, angle):
     """
     Takes as input an array of times and the angle in radians. Calculates the horizontal and vertical components of position and returns them as two numpy arrays.
@@ -45,8 +50,6 @@ def trajectory(time, angle):
     y = v_0 * np.sin(angle) * time - 0.5 * g * time**2
 
     return x, y
-
-
 
 
 if __name__ == "__main__":
