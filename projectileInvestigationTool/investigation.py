19aa75cf55ba216eb1214d0b0e7ec6a9e6da63be
Daniel Pierce <danp101@comcast.net>
2026-08-11 15:55:14 -0400

    Added functions to calculate total flight time and trajectory information

--- projectileInvestigationTool/investigation.py
+++ projectileInvestigationTool/investigation.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 
 def main():
     v0, launch_angle, y0, g = get_constants()
+    radian_angle = np.deg2rad(launch_angle)
+    time = flight_time(radian_angle, v0, g)
+
+    #Print time of flight
+    print(f"The total time of flight is {time} s")
 
 
 def get_constants():
@@ -17,9 +22,26 @@ def get_constants():
             g = float(input("Enter gravitational acceleration: "))
             break
         except ValueError:
-            print("Invalid value. Please try again.")
+            print("Invalid value(s). Please try again.")
 
     return v0, launch_angle, y0, g
+
+def flight_time(angle, v_0, g):
+    """
+    Takes as input an angle in radians and calculates the total flight time of a projectile
+    """
+    t = (2 * v_0 * np.sin(angle)) / g
+
+    return t
+
+def trajectory(time, angle, v_0, g, y0):
+    """
+    Takes as input an array of times and the angle in radians. Calculates the horizontal and vertical components of position and returns them as two numpy arrays.
+    """
+    x = v_0 * np.cos(angle) * time
+    y = y0 + v_0 * np.sin(angle) * time - 0.5 * g * time**2
+
+    return x, y
 
 
 if __name__ == "__main__":
