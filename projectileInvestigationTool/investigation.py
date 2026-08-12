0e6b742be24cb4cacf2576ce18de91940dc53d83
Daniel Pierce <danp101@comcast.net>
2026-08-11 20:36:09 -0400

    Added function for plotting and print statements for range and max height

--- projectileInvestigationTool/investigation.py
+++ projectileInvestigationTool/investigation.py
@@ -7,10 +7,27 @@ import matplotlib.pyplot as plt
 def main():
     v0, launch_angle, y0, g = get_constants()
     radian_angle = np.deg2rad(launch_angle)
-    time = flight_time(radian_angle, v0, g)
+    time_final = flight_time(radian_angle, v0, g)
+    time = np.linspace(0, time_final, 100)
 
     #Print time of flight
-    print(f"The total time of flight is {time} s")
+    print(f"The total time of flight is {time_final} s")
+
+    #Find trajectory
+    x, y = trajectory(time, radian_angle, v0, g, y0)
+
+    #Print range (based on available sample points)
+    print(f"The total range is {np.max(x)} m")
+
+    #Print Max height (based on available sample points)
+    print(f"The maximum height is {np.max(y)} m")
+
+    #Plot Position vs. Time
+    position_time_plot(time, y)
+
+    #Plot vertical velocity vs. Time
+
+    #Plot Trajectory
 
 
 def get_constants():
@@ -42,6 +59,15 @@ def trajectory(time, angle, v_0, g, y0):
     y = y0 + v_0 * np.sin(angle) * time - 0.5 * g * time**2
 
     return x, y
+
+def position_time_plot(time, y):
+    fig, ax = plt.subplots()
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Position (m)")
+    ax.set_title("Position vs. Time")
+    ax.plot(time, y, label = "Position")
+    ax.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
