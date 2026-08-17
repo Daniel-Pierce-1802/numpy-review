25b32266a9331b727fefdc31a0912b8fb4dc4321
Daniel Pierce <danp101@comcast.net>
2026-08-17 15:43:20 -0400

    Initial commit

---
+++ bisection/bisection.py
@@ -0,0 +1,19 @@
+# Author: Daniel J. Pierce
+# Date: 2026-08-16
+
+import numpy as np 
+
+def main():
+    ...
+
+def f(x):
+    return x**2 - x - 2
+
+
+def bisection(function, left, right, tolerance):
+    ...
+
+
+
+if __name__ == "__main__":
+    main()
