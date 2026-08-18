d4bcdcfdc340251375a3ed1ec3aa942c3a1e7f52
Daniel Pierce <danp101@comcast.net>
2026-08-17 21:18:10 -0400

    Fixed issue with appending to plotting arrays

--- euler/euler.py
+++ euler/euler.py
@@ -11,24 +11,24 @@ import numpy as np
 def main():
     # Define initial conditions
 
-    y0 = 10
+    y0 = 10.0
     k = 0.5
     dt = 0.1
 
     # Initialize arrays for plotting
 
-    t_numerical, y_numerical = [], []
+    t_numerical, y_numerical = [0.0], [y0]
     t = 0.0
 
     # Euler's Method
     y_current = y0
 
     while t < 1.0:
-        y_next = y_current + dt * -k * y_current
+        y_next = y_current + dt * (-k * y_current)
         y_current = y_next
         y_numerical.append(y_current)
-        t_numerical.append(t)
         t += dt
+        t_numerical.append(t)
 
     # Plot Euler's Method
     fig, ax = plt.subplots()
@@ -36,6 +36,7 @@ def main():
     ax.set_xlabel("Time")
     ax.set_ylabel("y")
     ax.plot(t_numerical, y_numerical, label = "Euler's Method")
+    ax.legend()
     plt.show()
     
     
