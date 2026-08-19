fa940ac31e3b89bba9005c8b16cf4cff49d30c29
Daniel Pierce <danp101@comcast.net>
2026-08-19 18:52:07 -0400

    Grouped Euler Method into function and plotted for multiple time step values

--- euler/euler.py
+++ euler/euler.py
@@ -2,8 +2,7 @@
 # Date: 2026-08-17
 
 import matplotlib.pyplot as plt
-import numpy as np 
-
+import numpy as np
 
 # Solving dy/dt = -ky
 
@@ -13,15 +12,33 @@ def main():
 
     y0 = 10.0
     k = 0.5
-    dt = 0.1
 
     # Initialize arrays for plotting
 
-    t_numerical, y_numerical = [0.0], [y0]
     t_analytical = np.linspace(0, 1, 100)
     y_analytical = y0 * np.exp(-k * t_analytical)
 
-    # Euler's Method
+    # Euler's Method for multiple timesteps & plot against analytical solution
+
+    time_steps = np.logspace(0, -5, 6)
+
+    fig, ax = plt.subplots()
+    ax.set_title(r"Numerical Solution to $dy/dt = -ky$")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("y")
+    ax.plot(t_analytical, y_analytical, label="Analytical Solution")
+
+    for dt in time_steps:
+        t_numerical, y_numerical = eulers_method(y0, k, dt)
+        ax.plot(t_numerical, y_numerical, label=f"Euler's Method (dt = {dt})")
+
+    ax.legend()
+    plt.show()
+
+
+def eulers_method(y0, k, dt):
+
+    t_numerical, y_numerical = [0.0], [y0]
     t = 0.0
     y_current = y0
 
@@ -32,18 +49,7 @@ def main():
         t += dt
         t_numerical.append(t)
 
-    # Plot Euler's Method against analytical solution
-    fig, ax = plt.subplots()
-    ax.set_title(r"Numerical Solution to $dy/dt = -ky$")
-    ax.set_xlabel("Time")
-    ax.set_ylabel("y")
-    ax.plot(t_numerical, y_numerical, label = "Euler's Method")
-    ax.plot(t_analytical, y_analytical, label = "Analytical Solution")
-    ax.legend()
-    plt.show()
-    
-    
-
+    return t_numerical, y_numerical
 
 
 if __name__ == "__main__":
