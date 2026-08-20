9ff773e6beb5a065a764aedcb6e1bdec247e783f
Daniel Pierce <danp101@comcast.net>
2026-08-20 18:16:41 -0400

    Added absolute error calculation for both Euler and RK4

--- radioactiveDecay/radioactive.py
+++ radioactiveDecay/radioactive.py
@@ -12,11 +12,16 @@ def main():
     euler_N, euler_t = euler(0.01, 10, 1000)
     rk4_t, rk4_N = RK4(1000, 10)
 
-    # Compute analytical solution
+    # Compute analytical solution for plotting
     N_0 = 1000
     λ = 0.01
     t = np.linspace(0,100)
     analytical_solution = N_0 * np.exp(-λ*t)
+
+    # Calculate error for each
+    euler_error, rk4_error = calculate_error(euler_N, rk4_N, euler_t, rk4_t)
+    print(f"Maximum error for Euler: {np.max(euler_error)}")
+    print(f"Maximum error for RK4: {np.max(rk4_error)}")
 
     # Plot solutions
     fig, ax = plt.subplots()
@@ -40,7 +45,7 @@ def euler(λ, dt, N_0):
         t += dt
         t_numerical.append(t)
     
-    return N_numerical, t_numerical
+    return np.array(N_numerical), np.array(t_numerical)
 
 def RK4(N_current, dt):
     # Set Initial Conditions
@@ -58,13 +63,26 @@ def RK4(N_current, dt):
         t += dt
         t_numerical.append(t)
     
-    return t_numerical, N_numerical
+    return np.array(t_numerical), np.array(N_numerical)
 
 def fun(t, N_current):
     λ = 0.01
     fun = -λ * N_current
 
     return fun
+
+def analytical_function(λ, N_0, t):
+    return N_0 * np.exp(-λ * t)
+
+
+def calculate_error(E_numerical, R_numerical, euler_time, rk4_time):
+    analytical_euler = analytical_function(0.01, 1000, euler_time)
+    analytical_rk4 = analytical_function(0.01, 1000, rk4_time)
+    euler_error = np.abs(E_numerical - analytical_euler)
+    rk4_error = np.abs(R_numerical - analytical_rk4)
+
+    return euler_error, rk4_error
+
 
 
 
